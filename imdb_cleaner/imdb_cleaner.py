e890ad23a81c43a0e410e66575b0d5a32b99cae7
James Rennison <22213990+stiffneckjim@users.noreply.github.com>
2024-10-05 20:58:29 +0100

    Fixed issue with sys..maxsize triggering an error: `OverflowError: Python int too large to convert to C long` Moved function to remove IMDb `null` values out of tsv_to_csv function Updated tqdm progress bar to include more information. Catch and print errors caused by large values.

--- imdb_cleaner/imdb_cleaner.py
+++ imdb_cleaner/imdb_cleaner.py
@@ -5,13 +5,15 @@ import glob
 import tqdm
 
 
-def tsv_to_csv():
-    # Increase the field size limit
-    csv.field_size_limit(sys.maxsize)
+def replace_null(value):
+    # Function to replace '\N' which IMDb uses for null values with an empty string
+    return '' if value == '\\N' else value
 
-    # Function to replace '\N' with an empty string
-    def replace_null(value):
-        return '' if value == '\\N' else value
+
+def tsv_to_csv():
+    # Increase the field size limit to system max size: 9223372036854775807
+    # csv.field_size_limit(sys.maxsize)
+    csv.field_size_limit(10**9)
 
     # Create the export directory if it doesn't exist
     os.makedirs('export', exist_ok=True)
@@ -19,11 +21,16 @@ def tsv_to_csv():
     # Get the list of all .tsv files in the import directory
     tsv_files = glob.glob('import/*.tsv')
 
+    # Format for progress bar
+    bar_format = "Progress: {l_bar}{bar} | Completed: {n_fmt}/{total_fmt} | Time: [{elapsed}<{remaining}] | Speed: {rate_fmt}{postfix}"
     # Iterate through all .tsv files with a progress bar
-    for tsv_file in tqdm.tqdm(tsv_files, desc="Processing files"):
+    for tsv_file in tqdm.tqdm(tsv_files, bar_format=bar_format, desc="Processing files"):
         # Define the corresponding CSV file path in the export directory
         csv_file = os.path.join('export', os.path.basename(tsv_file).replace('.tsv', '.csv'))
         
+        # Display the current file name
+        print(f"Processing {tsv_file}")
+
         # Open the TSV file and the new CSV file with the correct encoding
         with open(tsv_file, 'r', encoding='utf-8') as tsv, open(csv_file, 'w', newline='', encoding='utf-8') as csvf:
             tsv_reader = csv.reader(tsv, delimiter='\t')
@@ -31,10 +38,18 @@ def tsv_to_csv():
 
             # Write each row from the TSV file to the CSV file
             for row in tsv_reader:
+                # Test for integers exceeding `sys.maxsize`
+                for item in row:
+                    try:
+                        value = int(item)
+                        if value > sys.maxsize:
+                            print(f"Large value found: {value}")
+                    except ValueError:
+                        pass                
                 # Replace '\N' in each field
                 new_row = [replace_null(field) for field in row]
                 csv_writer.writerow(new_row)
-
+                
     print("Conversion complete!")
 
 
