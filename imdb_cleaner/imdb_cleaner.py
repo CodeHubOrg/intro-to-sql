b8d4291eafce233d6bfb5ccadf245a7bdb1cca61
James Rennison <22213990+stiffneckjim@users.noreply.github.com>
2024-10-08 18:09:21 +0000

    Add/clean invalid values (#2)
    * Added pandas
    Chunking up the TSV file into 1,000 rows.
    
    * Swapped csv module for pandas.
    Chunking up the TSV file into 1,000 row chunks.
    
    * Fixed bug in the way the remaining rows was being calculated.
    
    * Deleted test for max size supported by `csv` module.

--- imdb_cleaner/imdb_cleaner.py
+++ imdb_cleaner/imdb_cleaner.py
@@ -1,61 +1,49 @@
 """Clean data from IMDb site"""
 
-import csv
-import sys
-import ctypes
+import pandas as pd
 from tqdm import tqdm
 
 
-def replace_null(value):
+def replace_null(dataframe):
     """Function to replace '\\N' which IMDb uses for null values with an empty string"""
-    return "" if value == "\\N" else value
+    dataframe.replace("\\N", "", inplace=True)
 
 
 def tsv_to_csv(tsv_file, csv_file):
     """Take an IMDb TSV file, clean the rows and output a CSV file"""
-    # Calculate maximum size of long in Windows (32-bit?) and assign it to CSV field_size_limit
-    max_long = 2 ** (8 * ctypes.sizeof(ctypes.c_long) - 1) - 1
-    csv.field_size_limit(max_long)
-
     # Count rows in file
     with open(tsv_file, "r", encoding="utf-8") as file:
         num_rows = sum(1 for row in file)
 
-    # Open the TSV file and the new CSV file with the correct encoding
-    with open(tsv_file, "r", encoding="utf-8") as tsv, open(
-        csv_file, "w", newline="", encoding="utf-8"
-    ) as csvf:
-        tsv_reader = csv.reader(tsv, delimiter="\t")
-        csv_writer = csv.writer(csvf)
-        bar_format = (
-            "Progress: {l_bar}{bar} | Completed: {n_fmt}/{total_fmt} "
-            "| Time: [{elapsed}]"
-        )
+    # Read the TSV file in chunks
+    chunksize = 1000
+    chunks = pd.read_csv(tsv_file, sep="\t", chunksize=chunksize)
+    bar_format = (
+        "Progress: {l_bar}{bar} | Completed: {n_fmt}/{total_fmt} | Time: [{elapsed}]"
+    )
 
-        with tqdm(
-            total=num_rows,
-            bar_format=bar_format,
-            desc=f"Processing {csv_file}",
-            leave=False,
-        ) as rows_progress:
-            # Write each row from the TSV file to the CSV file
-            for i, row in enumerate(tsv_reader):
-                # Test for integers exceeding `sys.maxsize`
-                for item in row:
-                    try:
-                        value = int(item)
-                        if value > sys.maxsize:
-                            print(f"Large value found: {value}")
-                    except ValueError:
-                        pass
-                # Replace '\N' in each field
-                new_row = [replace_null(field) for field in row]
-                csv_writer.writerow(new_row)
+    with tqdm(
+        total=num_rows,
+        bar_format=bar_format,
+        desc=f"Processing {csv_file}",
+    ) as rows_progress:
+        # Write each row from the TSV file to the CSV file
+        for i, chunk in enumerate(chunks):
+            # Test for integers exceeding `sys.maxsize`
+            # Replace '\N' in each field
+            replace_null(chunk)
 
-                if (i + 1) % 100 == 0:
-                    rows_progress.update(100)
+            # Save the processed chunk to a CSV file
+            if i == 0:
+                # For the first chunk, write the header
+                chunk.to_csv(csv_file, index=False, mode="w")
+            else:
+                # For subsequent chunks, append without writing the header
+                chunk.to_csv(csv_file, index=False, mode="a", header=False)
 
-        # Ensure the progress bar completes if the total is not a multiple of 100
-        remaining = num_rows % 100
-        if remaining > 0:
-            rows_progress.update(remaining)
+            if (i + 1) * chunksize <= num_rows:
+                rows_progress.update(chunksize)
+            else:
+                # Ensure the progress bar completes if the total is not a multiple of 100
+                remaining = num_rows % chunksize
+                rows_progress.update(remaining)
