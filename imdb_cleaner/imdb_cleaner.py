64229af4fb96ca3f54969c90b2da11cdd7424314
James Rennison <22213990+stiffneckjim@users.noreply.github.com>
2024-10-11 14:18:21 +0100

    Now can load to CSV or SQLite database. Opens files to read from and write to once but still chunks up processing. Converts file names from IMDb to valid SQL table names. No longer reads entire file to get row count for progress bar (too slow). Load individual IMDb files, rather than anything with .tsv extension in a directory. Renamed some CLI arguments.

--- imdb_cleaner/imdb_cleaner.py
+++ imdb_cleaner/imdb_cleaner.py
@@ -1,49 +1,87 @@
 """Clean data from IMDb site"""
 
+import sqlite3
 import pandas as pd
 from tqdm import tqdm
 
 
-def replace_null(dataframe):
+def _replace_null(dataframe):
     """Function to replace '\\N' which IMDb uses for null values with an empty string"""
     dataframe.replace("\\N", "", inplace=True)
 
 
-def tsv_to_csv(tsv_file, csv_file):
+def tsv_to_csv(tsv_path, csv_path):
     """Take an IMDb TSV file, clean the rows and output a CSV file"""
-    # Count rows in file
-    with open(tsv_file, "r", encoding="utf-8") as file:
-        num_rows = sum(1 for row in file)
+    with open(tsv_path, "r", encoding="utf-8") as tsv_file:
+        # Count rows in file
+        chunksize = 1000
 
-    # Read the TSV file in chunks
-    chunksize = 1000
-    chunks = pd.read_csv(tsv_file, sep="\t", chunksize=chunksize)
-    bar_format = (
-        "Progress: {l_bar}{bar} | Completed: {n_fmt}/{total_fmt} | Time: [{elapsed}]"
-    )
+        # Create a progress bar
+        bar_format = "Progress: {l_bar}{bar} | Completed: {n_fmt} | Time: [{elapsed}]"
+        rows_progress = tqdm(
+            bar_format=bar_format,
+            desc=f"Processing {tsv_path}",
+        )
 
-    with tqdm(
-        total=num_rows,
-        bar_format=bar_format,
-        desc=f"Processing {csv_file}",
-    ) as rows_progress:
-        # Write each row from the TSV file to the CSV file
-        for i, chunk in enumerate(chunks):
-            # Test for integers exceeding `sys.maxsize`
-            # Replace '\N' in each field
-            replace_null(chunk)
+        with open(csv_path, "w", encoding="utf-8") as csv_file:
+            # Read the TSV file in chunks
+            # Write each row from the TSV file to the CSV file
+            for i, chunk in enumerate(
+                pd.read_csv(tsv_file, sep="\t", chunksize=chunksize)
+            ):
+                # Replace '\N' in each field
+                _replace_null(chunk)
 
-            # Save the processed chunk to a CSV file
-            if i == 0:
-                # For the first chunk, write the header
-                chunk.to_csv(csv_file, index=False, mode="w")
-            else:
-                # For subsequent chunks, append without writing the header
-                chunk.to_csv(csv_file, index=False, mode="a", header=False)
+                if i == 0:
+                    # For the first chunk, write the header
+                    chunk.to_csv(csv_file, index=False, mode="w")
+                else:
+                    # For subsequent chunks, append without writing the header
+                    chunk.to_csv(csv_file, index=False, mode="a", header=False)
 
-            if (i + 1) * chunksize <= num_rows:
-                rows_progress.update(chunksize)
-            else:
-                # Ensure the progress bar completes if the total is not a multiple of 100
-                remaining = num_rows % chunksize
-                rows_progress.update(remaining)
+                rows_progress.update(len(chunk))
+
+
+def tsv_to_sqlite(tsv_path, db_file, db_table):
+    """Take an IMDb TSV file, clean the rows and output to a SQLite database"""
+    with open(tsv_path, "r", encoding="utf-8") as tsv_file:
+        # Count rows in file
+        chunksize = 1000
+
+        # Create a progress bar
+        bar_format = "Progress: {l_bar}{bar} | Completed: {n_fmt} | Time: [{elapsed}]"
+        rows_progress = tqdm(
+            bar_format=bar_format,
+            desc=f"Processing {tsv_path}",
+        )
+        db_conn = sqlite3.connect(db_file)
+
+        with db_conn:
+            # Read the TSV file in chunks
+            # Write each row from the TSV file to the CSV file
+            for i, chunk in enumerate(
+                pd.read_csv(tsv_file, sep="\t", chunksize=chunksize)
+            ):
+                # Replace '\N' in each field
+                _replace_null(chunk)
+
+                if i == 0:
+                    # For the first create or replace the table
+                    chunk.to_sql(
+                        db_table,
+                        db_conn,
+                        if_exists="replace",
+                        index=False,
+                        chunksize=chunksize,
+                    )
+                else:
+                    # For subsequent chunks, append without writing the header
+                    chunk.to_sql(
+                        db_table,
+                        db_conn,
+                        if_exists="append",
+                        index=False,
+                        chunksize=chunksize,
+                    )
+
+                rows_progress.update(len(chunk))
--- load_imdb.py
+++ load_imdb.py
@@ -1,8 +1,8 @@
 """Load IMDb TSV files"""
 
 import os
-import glob
 import argparse
+import re
 from pprint import pprint
 from tqdm import tqdm
 from imdb_cleaner import imdb_cleaner
@@ -10,10 +10,28 @@ from imdb_cleaner import imdb_cleaner
 # Default configuration settings
 SETTINGS = {
     "input_dir": "import",
-    "output": "csv",
-    "csv_dir": "export",
+    "input_names": ["title.crew.tsv", "title.episode.tsv", "title.ratings.tsv"],
+    "output_format": "csv",
+    "output_dir": "export",
     "db_file": "imdb.sqlite",
 }
+
+
+def sanitise_table_name(table_name):
+    """Converts a string to a valid table name."""
+    # From Claude Haiku
+    # Remove special characters and spaces
+    table_name = re.sub(r"[^a-zA-Z0-9_]", "_", table_name)
+
+    # Convert to lowercase
+    table_name = table_name.lower()
+
+    # Truncate the name if necessary
+    max_length = 63  # Maximum table name length in SQLite
+    if len(table_name) > max_length:
+        table_name = table_name[:max_length]
+
+    return table_name
 
 
 def main():
@@ -22,40 +40,48 @@ def main():
     Option to write to a SQLite database instead."""
 
     # Check command-line arguments
-    if SETTINGS["output"] == "csv":
+    if SETTINGS["output_format"] == "csv":
         # Create the export directory if it doesn't exist
-        os.makedirs(SETTINGS["csv_dir"], exist_ok=True)
+        os.makedirs(SETTINGS["output_dir"], exist_ok=True)
 
-    elif SETTINGS["output"] == "sqlite":
+    elif SETTINGS["output_format"] == "sqlite":
         if not SETTINGS["db_file"]:
             raise ValueError(
                 "SQLite database file path must be provided when output format is 'sqlite'"
             )
+    else:
+        raise ValueError("Invalid output format: ", SETTINGS["output_format"])
 
-    # Get the list of all .tsv files in the import directory
-    input_path = os.path.join(SETTINGS["input_dir"], "*.tsv")
-    tsv_files = glob.glob(input_path)
+    pprint(SETTINGS)
+
     print("Loading files:")
-    pprint(tsv_files, indent=1)
+    pprint(SETTINGS["input_names"], indent=1)
 
     # Format for progress bar
-    bar_format = (
-        "Progress: {l_bar}{bar} | Completed: {n_fmt}/{total_fmt} | Time: [{elapsed}]"
-    )
+    bar_format = "Progress: {l_bar}{bar} | Completed: {n_fmt} | Time: [{elapsed}]"
 
-    # Iterate through all .tsv files with a progress bar
+    # Iterate through all TSV files with a progress bar
     with tqdm(
-        total=len(tsv_files), bar_format=bar_format, desc="Processing files"
+        total=len(SETTINGS["input_names"]),
+        bar_format=bar_format,
+        desc="Processing files",
     ) as files_progress:
-        for tsv_file in tsv_files:
-            # If output type is csv create CSV files
-            if SETTINGS["output"] == "csv":
+        for tsv_name in SETTINGS["input_names"]:
+            tsv_path = os.path.join(SETTINGS["input_dir"], tsv_name)
+            if SETTINGS["output_format"] == "csv":
+                # If output format is csv create CSV files
                 # Define the corresponding CSV file path in the export directory
-                csv_file = os.path.join(
-                    SETTINGS["csv_dir"],
-                    os.path.basename(tsv_file).replace(".tsv", ".csv"),
+                csv_path = os.path.join(
+                    SETTINGS["output_dir"], tsv_name.replace(".tsv", ".csv")
                 )
-                imdb_cleaner.tsv_to_csv(tsv_file=tsv_file, csv_file=csv_file)
+                imdb_cleaner.tsv_to_csv(tsv_path=tsv_path, csv_path=csv_path)
+            elif SETTINGS["output_format"] == "sqlite":
+                # If output format is SQLite create a SQLite database
+                # Define the corresponding table name in the SQLite database
+                db_table = sanitise_table_name(os.path.splitext(tsv_name)[0])
+                imdb_cleaner.tsv_to_sqlite(
+                    tsv_path=tsv_path, db_file=SETTINGS["db_file"], db_table=db_table
+                )
 
             files_progress.update(1)
 
@@ -74,7 +100,7 @@ if __name__ == "__main__":
         help="Directory that contains the input TSV files",
     )
     parser.add_argument(
-        "--output",
+        "--output_format",
         type=str,
         choices=["csv", "sqlite"],
         default="csv",
@@ -87,7 +113,7 @@ if __name__ == "__main__":
         help="Path to the SQLite database file (required if output is sqlite)",
     )
     parser.add_argument(
-        "--csv_dir",
+        "--output_dir",
         type=str,
         default="export",
         help="Path to the output CSV file (required if output is csv)",
@@ -95,10 +121,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Configuration settings
-    SETTINGS = {
-        "input_dir": args.input_dir,
-        "output": args.output,
-        "csv_dir": args.csv_dir,
-        "db_file": args.db_file,
-    }
+    SETTINGS["input_dir"] = args.input_dir
+    SETTINGS["output_format"] = args.output_format
+    SETTINGS["output_dir"] = args.output_dir
+    SETTINGS["db_file"] = args.db_file
+
     main()
