0e44456eff7a854c8af36fad40262dec1c34621f
James Rennison <22213990+stiffneckjim@users.noreply.github.com>
2025-02-03 23:01:56 +0000

    Decompress the gzip files into another directory.

--- download_imdb.py
+++ download_imdb.py
@@ -1,4 +1,7 @@
+"""Download dataset files from IMDb and then decompress them."""
+
 import os
+import gzip
 import argparse
 import requests
 from tqdm import tqdm
@@ -8,16 +11,15 @@ SETTINGS = {
     "data_location": "https://datasets.imdbws.com/",
     "download_dir": "downloads",
     "block_size": 1024,
-}
-
-DATA_FILES = {
-    # "name.basics.tsv.gz",
-    # "title.akas.tsv.gz",
-    # "title.basics.tsv.gz",
-    "title.crew.tsv.gz",
-    "title.episode.tsv.gz",
-    # "title.principals.tsv.gz",
-    "title.ratings.tsv.gz",
+    "data_files": {
+        # "name.basics.tsv.gz",
+        # "title.akas.tsv.gz",
+        # "title.basics.tsv.gz",
+        "title.crew.tsv.gz",
+        "title.episode.tsv.gz",
+        # "title.principals.tsv.gz",
+        "title.ratings.tsv.gz",
+    },
 }
 
 
@@ -48,28 +50,30 @@ def download_data_file(
         desc=data_file,
         bar_format=bar_format,
     ) as progress_bar:
-        with open(download_path, "wb") as file:
+        with open(download_path, "wb") as f_out:
             for data in response.iter_content(block_size):
                 progress_bar.update(len(data))
-                file.write(data)
+                f_out.write(data)
 
     if total_size != 0 and progress_bar.n != total_size:
         raise RuntimeError("Could not download file")
 
 
-if __name__ == "__main__":
+def decompress_file(file_name, input_dir, output_dir):
+    """Decompress the gzipped datasets"""
 
-    for file in tqdm(
-        DATA_FILES,
-        total=len(DATA_FILES),
-        desc=f"Downloading files from {SETTINGS["data_location"]}",
-    ):
-        download_data_file(
-            data_file=file,
-            data_location=SETTINGS["data_location"],
-            download_dir=SETTINGS["download_dir"],
-            block_size=SETTINGS["block_size"],
-        )
+    # Create the import directory if it doesn't exist
+    os.makedirs(output_dir, exist_ok=True)
+    input_file = os.path.join(input_dir, file_name)
+    # Remove the file extension for the uncompressed output file
+    output_file = os.path.join(output_dir, os.path.splitext(file_name)[0])
+
+    with gzip.open(input_file, "rb") as f_in:
+        with open(output_file, "wb") as f_out:
+            f_out.write(f_in.read())
+
+
+if __name__ == "__main__":
 
     # Parse command-line arguments
     parser = argparse.ArgumentParser(
@@ -88,8 +92,49 @@ if __name__ == "__main__":
         help="Directory to save downloaded files",
     )
     parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="import",
+        help="Directory to save unzipped dataset files",
+    )
+    parser.add_argument(
         "--block_size",
         type=int,
         default=1024,
         help="Block size when downloading files",
     )
+    parser.add_argument(
+        "--no_download", action="store_true", help="Don't download dataset files"
+    )
+    parser.add_argument(
+        "--no_decompress", action="store_true", help="Don't decompress dataset files"
+    )
+    args = parser.parse_args()
+
+    SETTINGS["data_location"] = args.data_location
+    SETTINGS["download_dir"] = args.download_dir
+    SETTINGS["output_dir"] = args.output_dir
+    SETTINGS["block_size"] = args.block_size
+
+    if not args.no_download:
+
+        for file in tqdm(
+            SETTINGS["data_files"],
+            total=len(SETTINGS["data_files"]),
+            desc=f"Downloading files from {SETTINGS["data_location"]}",
+        ):
+            download_data_file(
+                data_file=file,
+                data_location=SETTINGS["data_location"],
+                download_dir=SETTINGS["download_dir"],
+                block_size=SETTINGS["block_size"],
+            )
+
+    if not args.no_decompress:
+
+        for file in tqdm(
+            SETTINGS["data_files"],
+            total=len(SETTINGS["data_files"]),
+            desc=f"Decompressing files in {SETTINGS["download_dir"]}",
+        ):
+            decompress_file(file, SETTINGS["download_dir"], SETTINGS["output_dir"])
