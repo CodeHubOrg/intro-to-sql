d0121db43fe6d9dea89c1ded1f4909702e058ea6
stiffneckjim <22213990+stiffneckjim@users.noreply.github.com>
2025-03-03 10:37:37 +0000

    Split "known for" titles into a separate table.

--- name_basics_data.py
+++ name_basics_data.py
@@ -19,7 +19,25 @@ class NameBasicsData(IMDbData):
     def __init__(self, init_df):
         df_name = "name_basics"
         super().__init__(init_df, df_name)
-        # Clean the data and load it into a DataFrame
+        self.desired_columns = [
+            "primaryName",
+            "birthYear",
+            "deathYear",
+        ]
+        # Assign an index to the DataFrame
+        self.data_frames[df_name].set_index("nconst", inplace=True)
+        # Explode one compound columns and store as a new table. The other compound column is split
+        # into multiple columns
         self.data_frames[df_name] = self.split_columns(
-            self.data_frames[df_name], ["primaryProfession", "knownForTitles"]
+            self.data_frames[df_name], ["primaryProfession"]
         )
+        # Convert knownForTitles to a column of lists
+        self.data_frames[df_name]["knownForTitles"] = self.data_frames[df_name][
+            "knownForTitles"
+        ].str.split(",")
+        # Explode the knownForTitles column into a new data-frame and drop the other columns
+        known_for_titles = self.data_frames[df_name].explode("knownForTitles")
+        known_for_titles = known_for_titles["knownForTitles"]
+        # Drop the knownForTitles column from the name_basics data-frame
+        self.data_frames[df_name] = self.data_frames[df_name][self.desired_columns]
+        self.data_frames["known_for_titles"] = known_for_titles
