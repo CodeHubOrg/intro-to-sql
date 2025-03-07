3a03a78106d74723b3f27890e6c0720c83bde0b3
James Rennison <22213990+stiffneckjim@users.noreply.github.com>
2025-03-07 16:12:41 +0000

    Installing packages using devcontainer.json features instead of apt-get. Installing git-lfs as an apt package, because devcontainer feature was throwing errors. Uncompressing the sqlite database.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,11 +2,16 @@
 echo "Updating and upgrading Ubuntu packages"
 sudo apt-get update
 sudo apt-get upgrade -y
-echo "Installing build-essential, wget, gzip, and pipx Ubuntu packages"
-sudo apt-get install -y build-essential wget gzip pipx npm
+# echo "Installing build-essential, wget, gzip, and pipx Ubuntu packages"
+# sudo apt-get install -y build-essential wget gzip pipx npm
+# Feature to install git lfs fails so adding it here
+# git lfs install
+git lfs pull
 pipx ensurepath
 echo "Installing poetry and rust-just pipx packages"
 pipx install poetry rust-just
 echo "Using poetry to create a virtual environment"
 poetry install
 npm install sqlite3@5.1.7
+# git lfs pull
+gunzip -q /workspaces/intro-to-sql/prebuilt/imdb.sqlite.gz
