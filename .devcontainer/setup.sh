82adc14fa4dac040dc01c9310855e28d7f0fbdea
James Rennison <22213990+stiffneckjim@users.noreply.github.com>
2025-03-07 10:41:38 +0000

    Added npm to apt packages and sqlite3@5.1.7 to npm packages

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,9 +3,10 @@ echo "Updating and upgrading Ubuntu packages"
 sudo apt-get update
 sudo apt-get upgrade -y
 echo "Installing build-essential, wget, gzip, and pipx Ubuntu packages"
-sudo apt-get install -y build-essential wget gzip pipx
+sudo apt-get install -y build-essential wget gzip pipx npm
 pipx ensurepath
 echo "Installing poetry and rust-just pipx packages"
 pipx install poetry rust-just
 echo "Using poetry to create a virtual environment"
 poetry install
+npm install sqlite3@5.1.7
