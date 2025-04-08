c6f8038ccf59eaa8ec7a0a972d0ed3381a918f78
James Rennison <22213990+stiffneckjim@users.noreply.github.com>
2025-04-08 19:47:22 +0100

    15 missing packages in codespaces instance (#16)
    * Added a devcontainer.json file to configure codespaces instance.
    Added cspell extension files to .gitignore
    
    * Moved devcointer.json to .devcontainer folder.
    
    * Run apt-get as sudo
    Rename development container
    
    * Now store multiple data_frames in a data object, as there can be more than one table.
    Each data-frame is stored in a dict with name of the table in the database as the key.
    No longer auto-generating the table names. It's now bespoke for each data file.
    
    * Moved the class for processing and storing title.basics.tsv to a new file.
    Finally fixed the issue with not filtering adult titles - seems casting to integer doesn't work very well.
    
    * Removed python script for downloading data from IMDb.
    
    * Updated CLI instructions for load_imdb.py
    
    * Ignore uncompressed sqlite database.
    
    * Installing packages using devcontainer.json features instead of apt-get.
    Installing git-lfs as an apt package, because devcontainer feature was throwing errors.
    Uncompressing the sqlite database.
    
    * Added sqlite3 package to apt-packages feature in devcontainer.json
    
    * Restored accidentally removed compressed file.
    Keep the compressed DB after decompressing it
    
    * Adding workspace settings.json as it has details of the SQLTools connection to the imdb.sqlite database.
    
    * Fixed SQLTools connection

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,10 +2,16 @@
 echo "Updating and upgrading Ubuntu packages"
 sudo apt-get update
 sudo apt-get upgrade -y
-echo "Installing build-essential, wget, gzip, and pipx Ubuntu packages"
-sudo apt-get install -y build-essential wget gzip pipx
+# echo "Installing build-essential, wget, gzip, and pipx Ubuntu packages"
+# sudo apt-get install -y build-essential wget gzip pipx npm
+# Feature to install git lfs fails so adding it here
+# git lfs install
+git lfs pull
 pipx ensurepath
 echo "Installing poetry and rust-just pipx packages"
 pipx install poetry rust-just
 echo "Using poetry to create a virtual environment"
+# Install the python environment used for building the database
 poetry install
+# git lfs pull
+gunzip -k -q /workspaces/intro-to-sql/prebuilt/imdb.sqlite.gz
